26a078bffd6aef47a99ee779d4f2c52f9688d31a
Lyuda2500 <klimenchukluda@gmail.com>
2021-11-26 23:45:29 +0200

    Alpha
    Все работает, кроме глубины местами и отслеживание позиции при отпускании вверху.

--- js/game.js
+++ js/game.js
@@ -134,21 +134,30 @@ var WorldScene = new Phaser.Class({
     this.dragObj = targets[0];
     if (this.dragObj instanceof Phaser.GameObjects.Sprite) {
       this.input.off("pointerdown", this.startDrag, this);
+      //секция обработки стартовых плейсхолдеров (левый верхний угол)
+      //Если взаимодействуем с колодой
       if (this.dragObj.x == x1 && this.dragObj.y == y1) {
+        //Если в колоде кончились карты
         if (this.dragObj.name == "placeholder") {
+          //Перекидываем карты назад в колоду
           while (this.deck0.length > 1) {
             this.crd.push(this.deck0[this.deck0.length - 1].name);
             this.deck0[this.deck0.length - 1].destroy();
             this.deck0.pop();
           }
+          //Возвращаем рубашку
           this.shirt[0].visible = true;
           this.deck0[0].plo = 0;
         }
+        //Секция вытаскивания карт из колоды
+        //Если в колоде есть карты (есть рубашка - рубашка пропадает когда карты кончились)
         if (this.dragObj.name == "shirt") {
+          //Проверяем, если в колоде кончиличь карты - убираем рубашку
           if (this.crd.length == 0) {
             this.shirt[0].visible = false;
           } else {
-            this.deck0[0].plo += 1;
+            //Иначе продолжаем вытаскивать карты из колоды
+            this.deck0[0].plo += 1; //Необходимо для правильной работы логики перемещения между массивами
             let name = this.crd.pop();
             this.deck0.push(this.add.sprite(x2, y1, "cards", name));
             this.deck0[this.deck0.length - 1].name = name;
@@ -158,33 +167,28 @@ var WorldScene = new Phaser.Class({
             this.deck0[this.deck0.length - 1].pl = 0;
             this.deck0[this.deck0.length - 1].sx = x2;
             this.deck0[this.deck0.length - 1].sy = y1;
-            this.deck0[this.deck0.length - 1].pos = this.deck0.length;
+            this.deck0[this.deck0.length - 1].pos = this.deck0.length - 1;
             //console.log("FPC - " + this.deck12[this.deck12.length - 1].name);
             this.deck0[this.deck0.length - 1].setInteractive();
           }
         }
       } else {
-        // if (this.dragObj.x == x2 && this.dragObj.y == y1) {
-        //   this.dragObj.destroy();
-        // }
-        console.log("This - name: " + this.dragObj.name);
-        // console.log("PLO SD - " + eval("this.deck" + this.dragObj.pl + "[0].plo"));
-        //!-----------------------------------
+        //Иначе, - Если взаимодействуем с остальными картами
+
+        console.log("Klick - name: " + this.dragObj.name);
+        console.log("Klick - plo: " + eval("this.deck" + this.dragObj.pl + "[0].plo"));
+        console.log("Klick - pos" + this.dragObj.pos);
+        //!----------------------------------
         // var o;
         // eval("o = this.deck" + this.dragObj.pl + ".length - 1");
         // for (let i = 1; i <= o; i++) {
         //   console.log(eval("this.deck" + this.dragObj.pl + "[i].name"));
         // }
         //!----------------------------------
-        //this.scene.bringToTop(this.dragObj);
-        //game.scene.bringToTop(this.dragObj);
 
-        //!--OLD--
-        this.upcard = eval("this.deck" + this.dragObj.pl + ".length-1") - this.dragObj.pos;
-        //!--OLD--
-        // console.log("Upcard - " + this.upcard);
-        // console.log("Pos - " + this.dragObj.pos);
-        //podnimaet kartu nad ostalnymi esli ona posledniaja
+        this.upcard = eval("this.deck" + this.dragObj.pl + ".length-1") - this.dragObj.pos; //определяем количество карт сверху
+
+        //поднимает карту над остальными если она последняя
         if (this.upcard == 0) {
           this.dragObj.setDepth(2);
           //this.dragObj.scale = 1.05;
@@ -195,7 +199,7 @@ var WorldScene = new Phaser.Class({
           }
         }
 
-        this.input.on("pointermove", this.doDrag, this); //vkluchenie dvizheniia
+        this.input.on("pointermove", this.doDrag, this); //включение движения //TODO-----------------------------------------------------
       }
       this.input.on("pointerup", this.stopDrag, this);
     }
@@ -205,25 +209,25 @@ var WorldScene = new Phaser.Class({
     this.dragObj.x = pointer.x;
     this.dragObj.y = pointer.y;
     console.log("DODRAG - " + this.dragObj.name);
-    //kod nizhe tianet pachku
-    // esli zahvatil bolshe odnoj karty
+    //код ниже тянет пачку
+    //если захватил больше одной карты
     if (this.upcard > 0) {
-      for (let i = 1; i < this.upcard + 1; i++) /*цикл po kartam sverhu*/ {
+      for (let i = 1; i < this.upcard + 1; i++) /*цикл по картам сверху*/ {
         eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].x = pointer.x");
         eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].y = pointer.y + y_shift*i");
       }
     }
   },
 
+  //функция, которая выполняется, когда отпускаешь кнопку мыши при перетаскивании
   stopDrag(pointer) {
-    // function, kotoraja vypolnyaetcya, kogda otpuskaesh knopku myshy pri peretaskivanii
     this.input.on("pointerdown", this.startDrag, this);
     this.input.off("pointermove", this.doDrag, this);
     this.input.off("pointerup", this.stopDrag, this);
     this.dragObj.setDepth(0);
 
     if (pointer.y < 300) {
-      //verhnie placeholdery
+      //верхние плейсхолдеры
 
       // ace placeholder 1
       if (pointer.x > 499 && pointer.x < 660) {
@@ -241,7 +245,7 @@ var WorldScene = new Phaser.Class({
       if (pointer.x > 979) {
         this.finalmove(11, x7);
       }
-    } //nizhnie placeholdery
+    } //нижние плейсхолдеры
     else {
       //placeholder-1
       if (pointer.x < 180) {
@@ -274,35 +278,50 @@ var WorldScene = new Phaser.Class({
     }
     //this.dragObj.scale = 1;
   },
-  //metod of migration cards between arrays
+
+  //метод миграции карт между массивами
   peremeschenie(pl_out, xx) {
-    var obj;
+    //pl_out - массив, в который переносим
+    //pl - массив, из которого переносим
+    var obj; //карта(обьект) - на которую кладем
     var pl = this.dragObj.pl;
     eval("obj = this.deck" + pl_out + "[this.deck" + pl_out + ".length - 1]");
+    //Если цвета не совпадают или кладем на плейсхолдер
     if (obj.color != this.dragObj.color || obj.name == "placeholder_14") {
-      //esli ves menshe na 1
-      if (obj.weight == this.dragObj.weight + 1) {
-        eval("this.deck" + pl_out + "[0].plo+=1");
+      //если вес меньше на 1
+      if (this.dragObj.weight == obj.weight - 1) {
+        eval("this.deck" + pl_out + "[0].plo+=1"); //плюсуем счетчик открытых карт
         this.dragObj.x = xx;
         this.dragObj.y = y2 + eval("this.deck" + pl_out + ".length - 1") * y_shift;
+        //запоминаем стартовые координаты
         this.dragObj.sx = this.dragObj.x;
         this.dragObj.sy = this.dragObj.y;
 
-        //kod nizhe tianet pachku
-        // esli zahvatil bolshe odnoj karty
+        //код ниже тянет пачку
+        //если захватил больше одной карты
+        var l = eval("this.deck" + pl_out + ".length"); //запоминаем позицию перемещения карты, за которую тянем до начала цикла
         if (this.upcard > 0) {
           console.log("Upcard - " + this.upcard);
-          var l = eval("this.deck" + pl_out + ".length");
-          for (let i = 1; i < this.upcard + 1; i++) /*цикл po kartam sverhu*/ {
-            eval("this.deck" + pl_out + "[0].plo += 1");
-            eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].x = xx");
-            eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].y = this.dragObj.y + y_shift*i");
-            eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].sx = xx");
-            eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].sy = this.dragObj.y + y_shift*i");
-            eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].setDepth(0)");
-            // eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].scale = 1");
+          console.log("Drag - Pos - " + this.dragObj.pos);
+          //console.log("Length - " + eval("this.deck" + this.dragObj.pl + ".length"));
+          for (let i = 1; i < this.upcard + 1; i++) /*цикл по картам сверху*/ {
+            //todo **************** */
+            console.log("pl - " + pl);
+            console.log("Up-" + i + "-pos: " + eval("this.deck" + pl + "[this.dragObj.pos + i].pos"));
+            //todo **************** */
+            //! Теряется карта как объект в цикле !!!
+
+            eval("this.deck" + pl + "[this.dragObj.pos + i].x = xx");
+            eval("this.deck" + pl + "[this.dragObj.pos + i].y = this.dragObj.y + y_shift*i");
+            eval("this.deck" + pl + "[this.dragObj.pos + i].sx = xx");
+            eval("this.deck" + pl + "[this.dragObj.pos + i].sy = this.dragObj.y + y_shift*i");
+            eval("this.deck" + pl + "[this.dragObj.pos + i].setDepth(0)");
+          }
+          for (let i = 1; i < this.upcard + 1; i++) /*цикл по картам сверху*/ {
+            eval("this.deck" + pl_out + "[0].plo += 1"); //плюсуем счетчик открытых карт
+            //eval("this.deck" + this.dragObj.pl + "[this.dragObj.pos + i].scale = 1");
             //Perenos
-            let temp = l + 1 + this.upcard - i;
+            let temp = l + this.upcard + 1 - i;
             console.log("Befor - " + eval("this.deck" + pl_out + ".length - i"));
             eval("this.deck" + pl_out + "[temp] = this.deck" + pl + ".pop()"); //perenos mejdu massivami
             console.log("1 = " + temp + ", 2 = " + eval("this.deck" + pl_out + ".length - i"));
@@ -310,28 +329,31 @@ var WorldScene = new Phaser.Class({
             eval("this.deck" + pl + "[0].plo -= 1 "); //minusuem schetchik otkrytyj kart
             eval("this.deck" + pl_out + "[temp].pl = pl_out");
           }
-
+          console.log("After - " + eval("this.deck" + pl_out + ".length - 1"));
           eval("this.dragObj.pos = l");
           eval("this.deck" + pl_out + "[l] = this.deck" + pl + ".pop()"); //perenos mejdu massivami
           eval("this.deck" + pl + "[0].plo -= 1"); //minusuem schetchik otkrytyj kart
           this.dragObj.pl = pl_out;
         } else {
           //Perenos
-          eval("this.dragObj.pos = this.deck" + pl_out + ".length");
+          console.log("Pos-1card befor - " + this.dragObj.pos);
+          this.dragObj.pos = l;
+          console.log("Pos-1card after - " + this.dragObj.pos);
           eval("this.deck" + pl_out + ".push(this.deck" + pl + ".pop())"); //perenos mejdu massivami
+          console.log("Pos-in-massive- " + eval("this.deck" + pl_out + "[this.deck" + pl_out + ".length-1].pos"));
           eval("this.deck" + pl + "[0].plo -= 1"); //minusuem schetchik otkrytyj kart
           this.dragObj.pl = pl_out;
         }
         //открываем карту - убираем рубашку, если она есть, из placeholdera из которого взяли карту
         if (eval("this.deck" + pl + "[0].plc > 0")) {
-          console.log("PLC > 0");
-          console.log("PLO = " + eval("this.deck" + pl + "[0].plo"));
+          // console.log("PLC > 0");
+          // console.log("PLO = " + eval("this.deck" + pl + "[0].plo"));
           if (eval("this.deck" + pl + "[0].plo") == 0) {
-            console.log("Ona");
+            // console.log("Ona");
             eval("this.deck" + pl + "[0].plo += 1"); //vozvrashaem nazad schetchik, potomu chto otkryli rubashku
             eval("this.deck" + pl + "[0].plcc -= 1");
             eval("this.deck" + pl + "[this.deck" + pl + ".length - 1].setInteractive()");
-            console.log("PLO2 = " + eval("this.deck" + pl + "[0].plo"));
+            // console.log("PLO2 = " + eval("this.deck" + pl + "[0].plo"));
             this.remove_shirt(pl);
           }
         }
@@ -460,7 +482,6 @@ var WorldScene = new Phaser.Class({
       eval("this.deck" + i + "[0].weight = 14");
       do {
         eval("this.deck" + i + "[0].plo +=1");
-        console.log("FIRST-PLO - " + eval("this.deck" + i + "[0].plo"));
         name = this.crd[this.crd.length - 1]; //zapominaem nazvanie kart vyhodiashih iz kolody v igru
         eval("this.deck" + i + "[j] = this.add.sprite(posx, posy, 'cards', this.crd.pop())"); //! .pop() zabiraet kartu iz kolody
         eval("this.deck" + i + "[j].name = name"); //imia karty
